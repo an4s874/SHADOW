908c3cb058b99f7f89b66798aedcb10c1dd89682
an4s874 <50658369+an4s874@users.noreply.github.com>
2019-06-19 10:17:07 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['478192028279111690'];
 const db = require('quick.db');
-const premium = ['470896018603376640']
+const premium = ['478192028279111690']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = '>';
+const prefix = '!';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['💜',' anas to empty 💜'];
+    var setGame = ['5665','BY 5665 '];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -3524,4 +3524,4 @@ client.on('guildMemberAdd', member => {
 return channel.send("")
     }
     )});
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.NTc5MDA4MDg5NzE0MzI3NTUy.XQpmKQ.v2rEB-wwnO7a7BWSx2r3hYk8Qvc)
